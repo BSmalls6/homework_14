d5634193d1fc504a483a53ab0c6ae051a2ee7844
BSmalls6 <smallwoodbrennan@gmail.com>
2020-01-30 16:47:45 -0800

    adding more files and file structure

---
+++ Develop/public/webpack.config.js
@@ -0,0 +1,55 @@
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: {
+    app: "../server.js",
+    index: "/index.js",
+    db: "/db.js"
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new SWPrecacheWebpackPlugin({
+      cacheId: "my-domain-cache-id",
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: "service-worker.js",
+      minify: true,
+      staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/]
+    }),
+    new WebpackPwaManifest({
+      name: "budget app",
+      short_name: "budgeter",
+      description: "An application that keep track of your budget and make updates to it online or offline",
+      background_color: "#F7FAFC",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [{
+        src: path.resolve("assets/images/icons/android-chrome-192x192.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
+    })
+  ]
+};
+
+module.exports = config;
